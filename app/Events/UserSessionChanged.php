80a5e2ea124b6e9e59c69696fc6ff0aa0c117d93
Pham Huu Truong <t.pham@sup-scs.de>
2021-04-19 11:05:49 +0200

    Using Laravel Listeners to Broadcast Changes on Users's Session
    BroadcastUserLoginNotification and BroadcastUserLogoutNotification will make the broadcast will the message.
    That message need to write without blank space.
    Then each Notification will be added to the EventServiceProvider.

--- app/Events/UserSessionChanged.php
+++ app/Events/UserSessionChanged.php
@@ -9,6 +9,7 @@ use Illuminate\Broadcasting\PrivateChannel;
 use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
 use Illuminate\Foundation\Events\Dispatchable;
 use Illuminate\Queue\SerializesModels;
+use Illuminate\Support\Facades\Log;
 
 class UserSessionChanged implements ShouldBroadcast
 {
@@ -36,6 +37,8 @@ class UserSessionChanged implements ShouldBroadcast
      */
     public function broadcastOn()
     {
+        Log::debug($this->message);
+        Log::debug($this->type);
         return new Channel('notifications');
     }
 }
